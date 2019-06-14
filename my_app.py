8517d97dc4133f8711f2057e68fff335e24deafa
AnnaPalarkina171 <annettepalatkina@gmail.com>
2019-06-14 15:28:55 +0300

    hw

--- my_app.py
+++ my_app.py
@@ -25,6 +25,6 @@ def index():
 if __name__ == '__main__':
     app.run(debug=False)
     app.debug = True
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
     app.run(host='0.0.0.0', port=port)
 
